e2f88914edce9c96f9ae520a2dc6d4334ee7f38d
silberth garcia <123778637+Silberth90@users.noreply.github.com>
2025-11-19 18:27:33 -0300

    Add files via upload

--- lambda/lambda_function.py
+++ lambda/lambda_function.py
@@ -5,19 +5,17 @@ glue = boto3.client("glue")
 
 def lambda_handler(event, context):
     try:
-        # Extraer info del evento S3
         bucket_name = event['Records'][0]['s3']['bucket']['name']
         object_key = event['Records'][0]['s3']['object']['key']
-        
+
         print(f"Nuevo archivo detectado: s3://{bucket_name}/{object_key}")
-        
-        # Ejecutar solo si es CSV
+
         if object_key.endswith(".csv"):
             response = glue.start_job_run(
                 JobName="athenalambda",
                 Arguments={
                     "--S3_BUCKET": bucket_name,
-                    "--BRONZE_PATH": f"s3://{bucket_name}/{object_key}",
+                    "--BRONZE_PATH": f"s3://{bucket_name}/bronze/",
                     "--SILVER_PATH": f"s3://{bucket_name}/silver/",
                     "--GOLD_PATH": f"s3://{bucket_name}/gold/"
                 }
@@ -27,14 +25,8 @@ def lambda_handler(event, context):
         else:
             print("El archivo no es CSV, no se dispara el Job.")
 
-        return {
-            "statusCode": 200,
-            "body": json.dumps("Lambda ejecutada correctamente.")
-        }
+        return {"statusCode": 200, "body": json.dumps("Lambda ejecutada correctamente.")}
 
     except Exception as e:
         print(f"Error: {str(e)}")
-        return {
-            "statusCode": 500,
-            "body": json.dumps({"error": str(e)})
-        }
+        return {"statusCode": 500, "body": json.dumps({"error": str(e)})}
